0888ab266c78a8ee5e35a6f80b3d9b3b303f447b
PhiAgent <princegyekyeaddai@gmail.com>
2023-12-25 12:34:27 -0500

    checkedExceptionHandling

--- 08. rest-api/contacts/starter/contacts/src/main/java/com/ltp/contacts/service/ContactServiceImpl.java
+++ 08. rest-api/contacts/starter/contacts/src/main/java/com/ltp/contacts/service/ContactServiceImpl.java
@@ -1,10 +1,12 @@
 package com.ltp.contacts.service;
 
+import java.util.List;
 import java.util.stream.IntStream;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.ltp.contacts.exception.NoContactException;
 import com.ltp.contacts.pojo.Contact;
 import com.ltp.contacts.repository.ContactRepository;
 
@@ -15,11 +17,16 @@ public class ContactServiceImpl implements ContactService {
     private ContactRepository contactRepository;
 
     @Override
-    public Contact getContactById(String id){
+    public Contact getContactById(String id) throws NoContactException{
         return contactRepository.getContact(findIndexById(id));
     }
 
-    // "a33d23fd-08c5-4be3-9df8-c495f389911d"
+    @Override
+    public List<Contact> getContacts(){
+        return contactRepository.getContacts();
+    }
+
+    // "c0e720af-3910-4441-90b2-0ad98fd489db"
 
     @Override
     public void saveContact(Contact contact){
@@ -27,20 +34,20 @@ public class ContactServiceImpl implements ContactService {
     }
 
     @Override
-    public void updateContact(String id, Contact contact){
+    public void updateContact(String id, Contact contact) throws NoContactException{
         contactRepository.updateContact(findIndexById(id), contact);
     }
 
     @Override
-    public void deleteContact(String id){
+    public void deleteContact(String id) throws NoContactException{
         contactRepository.deleteContact(findIndexById(id));
     }
 
-    private int findIndexById(String id) {
+    private int findIndexById(String id) throws NoContactException {
         return IntStream.range(0, contactRepository.getContacts().size())
             .filter(index -> contactRepository.getContacts().get(index).getId().equals(id))
             .findFirst()
-            .orElseThrow();
+            .orElseThrow(() -> new NoContactException());
     }
 
 }
