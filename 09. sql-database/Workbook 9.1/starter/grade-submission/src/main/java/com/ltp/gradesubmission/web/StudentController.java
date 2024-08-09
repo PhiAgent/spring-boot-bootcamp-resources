dce14fefc63d30275bf9b22cf7a879551c6d220d
PhiAgent <princegyekyeaddai@gmail.com>
2024-08-09 11:46:47 -0400

    deleteStudent

--- 09. sql-database/Workbook 9.1/starter/grade-submission/src/main/java/com/ltp/gradesubmission/web/StudentController.java
+++ 09. sql-database/Workbook 9.1/starter/grade-submission/src/main/java/com/ltp/gradesubmission/web/StudentController.java
@@ -2,11 +2,13 @@ package com.ltp.gradesubmission.web;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ltp.gradesubmission.entity.Student;
@@ -24,5 +26,10 @@ public class StudentController {
   @PostMapping("")
   public ResponseEntity<Student> saveStudent(@RequestBody Student student){
     return new ResponseEntity(student, HttpStatus.CREATED);
+  }
+
+  @DeleteMapping("")
+  public ResponseEntity<Student> deleteStudent(@RequestParam String id){
+    return new ResponseEntity(HttpStatus.NO_CONTENT);
   }
 }
