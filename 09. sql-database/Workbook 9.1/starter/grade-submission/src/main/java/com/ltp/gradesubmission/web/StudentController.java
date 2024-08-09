2a7501312675f412073bf96da75a2448a93ba33a
PhiAgent <princegyekyeaddai@gmail.com>
2024-08-09 11:37:28 -0400

    postStudent

--- 09. sql-database/Workbook 9.1/starter/grade-submission/src/main/java/com/ltp/gradesubmission/web/StudentController.java
+++ 09. sql-database/Workbook 9.1/starter/grade-submission/src/main/java/com/ltp/gradesubmission/web/StudentController.java
@@ -4,6 +4,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,5 +19,10 @@ public class StudentController {
   @GetMapping("/{id}")
   public ResponseEntity<Student> getStudent(@PathVariable Long id){
     return new ResponseEntity(HttpStatus.OK);
+  }
+
+  @PostMapping("")
+  public ResponseEntity<Student> saveStudent(@RequestBody Student student){
+    return new ResponseEntity(student, HttpStatus.CREATED);
   }
 }
