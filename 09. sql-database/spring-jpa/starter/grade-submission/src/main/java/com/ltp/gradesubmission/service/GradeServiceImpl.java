8f7e4ca3414a4cf9f31a6536a537607c0fe68933
PhiAgent <princegyekyeaddai@gmail.com>
2024-08-10 15:40:50 -0400

    updateGrade

--- 09. sql-database/spring-jpa/starter/grade-submission/src/main/java/com/ltp/gradesubmission/service/GradeServiceImpl.java
+++ 09. sql-database/spring-jpa/starter/grade-submission/src/main/java/com/ltp/gradesubmission/service/GradeServiceImpl.java
@@ -5,8 +5,10 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.ltp.gradesubmission.entity.Course;
 import com.ltp.gradesubmission.entity.Grade;
 import com.ltp.gradesubmission.entity.Student;
+import com.ltp.gradesubmission.repository.CourseRepository;
 import com.ltp.gradesubmission.repository.GradeRepository;
 import com.ltp.gradesubmission.repository.StudentRepository;
 
@@ -18,24 +20,31 @@ public class GradeServiceImpl implements GradeService {
 
     GradeRepository gradeRepository;
     StudentRepository studentRepository;
+    CourseRepository courseRepository;
 
     @Override
     public Grade getGrade(Long studentId, Long courseId) {
-        return gradeRepository.findByStudentId(studentId);
+        return gradeRepository.findByStudentIdAndCourseId(studentId, courseId);
     }
 
     @Override
     public Grade saveGrade(Grade grade, Long studentId, Long courseId) {
         // we find the student that matches that id
         Student student = studentRepository.findById(studentId).get();
+        Course course = courseRepository.findById(courseId).get();
         // the we associate that student to the grade before saving the grade
         grade.setStudent(student);
+        grade.setCourse(course);
         return gradeRepository.save(grade);
     }
 
     @Override
     public Grade updateGrade(String score, Long studentId, Long courseId) {
-        return null;
+        Grade grade = gradeRepository.findByStudentIdAndCourseId(studentId, courseId);
+        grade.setScore(score);
+        // spring jpa will update the grade rather than create a new one since
+        // the grade being saved already exists in the database
+        return gradeRepository.save(grade);
     }
 
     @Override
