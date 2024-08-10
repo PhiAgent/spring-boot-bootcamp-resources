63c128452baea85cf2eac2af93e8cd31587d91c7
PhiAgent <princegyekyeaddai@gmail.com>
2024-08-09 20:45:24 -0400

    mustAddServieAnnotationToSetClassImplAsBeanIfInterfaceHoweverNoNeedToAddAnnotatino

--- 09. sql-database/spring-jpa/starter/grade-submission/src/main/java/com/ltp/gradesubmission/service/StudentServiceImpl.java
+++ 09. sql-database/spring-jpa/starter/grade-submission/src/main/java/com/ltp/gradesubmission/service/StudentServiceImpl.java
@@ -3,14 +3,20 @@ package com.ltp.gradesubmission.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.ltp.gradesubmission.entity.Student;
 import com.ltp.gradesubmission.repository.StudentRepository;
 
+@Service
+// note that if you setup the service as a class, then you'd need to add the @Service annotation to set it up as a
+// bean. if you used an interface however, you'd not need to add the @Service annotation
+// that's why in the StudentRepository interface we dont' add @Repository annotation
 public class StudentServiceImpl implements StudentService {
 
     @Autowired
     StudentRepository studentRepository;
+
     @Override
     public Student getStudent(Long id) {
         return null;
