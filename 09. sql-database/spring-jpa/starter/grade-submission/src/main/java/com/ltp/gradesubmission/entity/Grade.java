1733f083719c0e082d888b8adb98aa846c271f54
PhiAgent <princegyekyeaddai@gmail.com>
2024-08-10 08:04:43 -0400

    setupGrade

--- 09. sql-database/spring-jpa/starter/grade-submission/src/main/java/com/ltp/gradesubmission/entity/Grade.java
+++ 09. sql-database/spring-jpa/starter/grade-submission/src/main/java/com/ltp/gradesubmission/entity/Grade.java
@@ -1,24 +1,30 @@
 package com.ltp.gradesubmission.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "grades")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Grade {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
     private Long id;
+    @Column(name = "score", nullable = false)
     private String score;
-
-    public Long getId() {
-        return this.id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getScore() {
-        return this.score;
-    }
-
-    public void setScore(String score) {
-        this.score = score;
-    }
 
 }
