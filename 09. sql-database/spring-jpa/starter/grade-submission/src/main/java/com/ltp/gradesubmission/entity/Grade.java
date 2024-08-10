03fb8f241c3a39a2e4287a681c3d2bd0e238363f
PhiAgent <princegyekyeaddai@gmail.com>
2024-08-10 08:30:00 -0400

    manyToOneAndJoinColumn

--- 09. sql-database/spring-jpa/starter/grade-submission/src/main/java/com/ltp/gradesubmission/entity/Grade.java
+++ 09. sql-database/spring-jpa/starter/grade-submission/src/main/java/com/ltp/gradesubmission/entity/Grade.java
@@ -5,6 +5,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
@@ -27,4 +29,15 @@ public class Grade {
     @Column(name = "score", nullable = false)
     private String score;
 
+    // A single student can have many grades and we define that using the annotation
+    // of ManyToOne
+    @ManyToOne
+    // Each grade will have a foriegn key that points to the student that owns that
+    // grade; to define this relationship, we'll use the joinColumn annotation
+    // we'll specify the name of the column in the foreign table(referencedColumnName)
+    // and the name that we'd like to name this foreign key column in our table(name)
+    @JoinColumn(name = "student_id", referencedColumnName = "id")
+    //each grade must be associated to a
+    // student. We define this association by defining a student field for grade
+    private Student student;
 }
