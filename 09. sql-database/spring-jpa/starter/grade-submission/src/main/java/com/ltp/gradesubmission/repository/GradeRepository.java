994c972049026f0862897cd6d79e348b27d95520
PhiAgent <princegyekyeaddai@gmail.com>
2024-08-10 15:40:31 -0400

    findByStudentIdAndCourseId

--- 09. sql-database/spring-jpa/starter/grade-submission/src/main/java/com/ltp/gradesubmission/repository/GradeRepository.java
+++ 09. sql-database/spring-jpa/starter/grade-submission/src/main/java/com/ltp/gradesubmission/repository/GradeRepository.java
@@ -1,12 +1,13 @@
 package com.ltp.gradesubmission.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.repository.CrudRepository;
 
 import com.ltp.gradesubmission.entity.Grade;
 
 public interface GradeRepository extends CrudRepository<Grade, Long>{
-  Grade findByStudentId(Long studentId);
+  Grade findByStudentIdAndCourseId(Long studentId, Long courseId);
   List<Grade> findByCourseId(Long courseId);
 }
