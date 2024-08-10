676bd9562141cd0dd95b2adb7d97969778b562f1
PhiAgent <princegyekyeaddai@gmail.com>
2024-08-10 12:05:17 -0400

    allArgsConstructorReplaceAutowired

--- 09. sql-database/spring-jpa/starter/grade-submission/src/main/java/com/ltp/gradesubmission/service/StudentServiceImpl.java
+++ 09. sql-database/spring-jpa/starter/grade-submission/src/main/java/com/ltp/gradesubmission/service/StudentServiceImpl.java
@@ -8,14 +8,25 @@ import org.springframework.stereotype.Service;
 import com.ltp.gradesubmission.entity.Student;
 import com.ltp.gradesubmission.repository.StudentRepository;
 
+import lombok.AllArgsConstructor;
+
 @Service
 // note that if you setup the service as a class, then you'd need to add the @Service annotation to set it up as a
 // bean. if you used an interface however, you'd not need to add the @Service annotation
 // that's why in the StudentRepository interface we dont' add @Repository annotation
+@AllArgsConstructor
 public class StudentServiceImpl implements StudentService {
 
-    @Autowired
     StudentRepository studentRepository;
+
+    // now if you create a constructor like below using studentRepository, you
+    // won't need to use the autowired annotation
+    // stringboot will automatically create the bean
+    // now using this argument, we can use the
+    // AllArgsConstructor from Lombok to accomplish same results and remove need for Autowired
+    // public StudentServiceImpl(StudentRepository studentRepository){
+    //     this.studentRepository = studentRepository;
+    // }
 
     @Override
     public Student getStudent(Long id) {
