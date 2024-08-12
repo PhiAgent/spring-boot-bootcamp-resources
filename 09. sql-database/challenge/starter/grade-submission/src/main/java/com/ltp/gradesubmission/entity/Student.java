835c814325c5acc35bd5cfce4c1134b63ff7d170
PhiAgent <princegyekyeaddai@gmail.com>
2024-08-12 12:25:49 -0400

    studentCourseManyToManyInit

--- 09. sql-database/challenge/starter/grade-submission/src/main/java/com/ltp/gradesubmission/entity/Student.java
+++ 09. sql-database/challenge/starter/grade-submission/src/main/java/com/ltp/gradesubmission/entity/Student.java
@@ -9,6 +9,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
@@ -40,9 +41,11 @@ public class Student {
     @NonNull
     @Column(name = "birth_date", nullable = false)
     private LocalDate birthDate;
-    
+
     @JsonIgnore
     @OneToMany(mappedBy = "student", cascade = CascadeType.ALL)
     private List<Grade> grades;
 
+    @ManyToMany(mappedBy = "students")
+    private List<Course> courses;
 }
