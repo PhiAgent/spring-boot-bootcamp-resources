c52e117187eba080873e9df41790b0721e2e50bd
PhiAgent <princegyekyeaddai@gmail.com>
2024-08-12 14:17:18 -0400

    StudentNotEnrolledException

--- 09. sql-database/challenge/starter/grade-submission/src/main/java/com/ltp/gradesubmission/service/GradeServiceImpl.java
+++ 09. sql-database/challenge/starter/grade-submission/src/main/java/com/ltp/gradesubmission/service/GradeServiceImpl.java
@@ -6,6 +6,7 @@ import com.ltp.gradesubmission.entity.Course;
 import com.ltp.gradesubmission.entity.Grade;
 import com.ltp.gradesubmission.entity.Student;
 import com.ltp.gradesubmission.exception.GradeNotFoundException;
+import com.ltp.gradesubmission.exception.StudentNotEnrolledException;
 import com.ltp.gradesubmission.repository.CourseRepository;
 import com.ltp.gradesubmission.repository.GradeRepository;
 import com.ltp.gradesubmission.repository.StudentRepository;
@@ -18,12 +19,12 @@ import org.springframework.stereotype.Service;
 @AllArgsConstructor
 @Service
 public class GradeServiceImpl implements GradeService {
-    
+
     GradeRepository gradeRepository;
     StudentRepository studentRepository;
     CourseRepository courseRepository;
 
-    
+
     @Override
     public Grade getGrade(Long studentId, Long courseId) {
         Optional<Grade> grade = gradeRepository.findByStudentIdAndCourseId(studentId, courseId);
@@ -34,6 +35,9 @@ public class GradeServiceImpl implements GradeService {
     public Grade saveGrade(Grade grade, Long studentId, Long courseId) {
         Student student = StudentServiceImpl.unwrapStudent(studentRepository.findById(studentId), studentId);
         Course course = CourseServiceImpl.unwrapCourse(courseRepository.findById(courseId), courseId);
+
+        checkIfStudentEnrolledInCourse(course, student);
+
         grade.setStudent(student);
         grade.setCourse(course);
         return gradeRepository.save(grade);
@@ -66,11 +70,16 @@ public class GradeServiceImpl implements GradeService {
     public List<Grade> getAllGrades() {
         return (List<Grade>) gradeRepository.findAll();
     }
-    
+
     static Grade unwrapGrade(Optional<Grade> entity, Long studentId, Long courseId) {
         if (entity.isPresent()) return entity.get();
         else throw new GradeNotFoundException(studentId, courseId);
     }
 
-
+    static Boolean checkIfStudentEnrolledInCourse(Course course, Student student) {
+        if (course.getStudents().contains(student))
+            return true;
+        else
+            throw new StudentNotEnrolledException(student.getId(), course.getId());
+    }
 }
