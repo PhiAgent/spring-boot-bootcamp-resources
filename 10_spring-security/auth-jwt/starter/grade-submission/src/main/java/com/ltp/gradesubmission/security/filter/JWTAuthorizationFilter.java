b45b42bbbbcc639904fa1667c6345fc018c7e541
PhiAgent <princegyekyeaddai@gmail.com>
2024-08-15 15:43:08 -0400

    JWTAuthorizationFilterNotes

--- 10_spring-security/auth-jwt/starter/grade-submission/src/main/java/com/ltp/gradesubmission/security/filter/JWTAuthorizationFilter.java
+++ 10_spring-security/auth-jwt/starter/grade-submission/src/main/java/com/ltp/gradesubmission/security/filter/JWTAuthorizationFilter.java
@@ -1,5 +1,10 @@
 package com.ltp.gradesubmission.security.filter;
 
+// In this class, we take the token and the secret key and generate a signature using
+// the algorithm, token header and payload and
+// compare the signature generated to the signature in the token
+// if the signature is valid, then we set the authentication object on the security context
+// holder. the security context holder stores details of who is authenticated
 public class JWTAuthorizationFilter {
-    
+
 }
