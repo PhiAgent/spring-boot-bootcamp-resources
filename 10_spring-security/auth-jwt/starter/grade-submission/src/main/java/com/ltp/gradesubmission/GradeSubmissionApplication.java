8a0715ea07f83c36d6d6b84a1417e58540c782e8
PhiAgent <princegyekyeaddai@gmail.com>
2024-08-21 13:32:02 -0400

    final edit on jwt authentication first time learning

--- 10_spring-security/auth-jwt/starter/grade-submission/src/main/java/com/ltp/gradesubmission/GradeSubmissionApplication.java
+++ 10_spring-security/auth-jwt/starter/grade-submission/src/main/java/com/ltp/gradesubmission/GradeSubmissionApplication.java
@@ -32,9 +32,9 @@ public class GradeSubmissionApplication implements CommandLineRunner {
             new Student("Harry Potter", LocalDate.parse(("1980-07-31"))),
             new Student("Ron Weasley", LocalDate.parse(("1980-03-01"))),
             new Student("Hermione Granger", LocalDate.parse(("1979-09-19"))),
-            new Student("Neville Longbottom", LocalDate.parse(("1980-07-30"))) 
+            new Student("Neville Longbottom", LocalDate.parse(("1980-07-30")))
         };
-		
+
 		for (int i = 0; i < students.length; i++) {
 			studentRepository.save(students[i]);
 		}
@@ -51,13 +51,11 @@ public class GradeSubmissionApplication implements CommandLineRunner {
 		for (int i = 0; i < courses.length; i++) {
 			courseRepository.save(courses[i]);
 		}
- 
+
 	}
 
 	@Bean
 	public BCryptPasswordEncoder bCryptPasswordEncoder() {
 		return new BCryptPasswordEncoder();
-	}	
-
-
+	}
 }
